b26e4b587ea151dadfe94acd5f60d73544849eaf
Soyo Solution <soyosolution@gmail.com>
2016-09-14 12:28:14 +0800

    Add more cases for samples

--- demo.php
+++ demo.php
@@ -6,13 +6,17 @@ try{
     $cropResizer->resizeKeepRatioByWidth(250, "samples/3105589762_keepratio_by_w.jpg");
     $cropResizer->resizeKeepRatioByHeight(250, "samples/3105589762_keepratio_by_h.jpg");
     $cropResizer->cropImg(250,123,100,100,"samples/3105589762_crop.jpg");
+    $cropResizer->cropImg(250,123,100,100,"samples/3105589762_crop_overflow1.jpg");
+    $cropResizer->cropImg(400,400,560,45,"samples/3105589762_crop_overflow2_right.jpg");
+    $cropResizer->cropImg(400,400,460,75,"samples/3105589762_crop_overflow2_bottom.jpg");
 
     $cropResizer2 = new CropResizer("samples\butterfly-wallpaper.jpeg");
     $cropResizer2->resizeKeepRatioByWidth(600, "samples\butterfly_w600_keepratio_by_w.jpg");
     $cropResizer2->resizeKeepRatioByHeight(150, "samples\butterfly_h150_keepratio_by_h.jpg");
     $cropResizer2->cropImg(256,160,500,500,"samples\butterfly_crop.jpg");
-    //$dst_w, $dst_h, $src_x, $src_y, $resultDirPath
-    $cropResizer2->cropImg(500,500,2500,1500,"samples\butterfly_crop_right.jpg");
+    $cropResizer2->cropImg(500,500,2460,1500,"samples\butterfly_crop_overflow1.jpg");
+    $cropResizer2->cropImg(500,500,2360,1500,"samples\butterfly_crop_overflow2_right.jpg");
+    $cropResizer2->cropImg(500,500,2460,1200,"samples\butterfly_crop_overflow3_bottom.jpg");
     
     echo 'Crop and resize finsihed, please check photos in "samples" files.';
 }catch (Exception $e){
